05cec07334350c74f5675f43ffd054bac3b05c78
InmookJeong <jeonginmook@gmail.com>
2024-05-29 00:06:17 +0900

    feat - Create HomeController and jsp files
    Create HomeController.
    Create home.jsp and member's home.jsp.

---
+++ tiles/src/main/java/kr/mook/tiles/home/controller/HomeController.java
@@ -0,0 +1,28 @@
+package kr.mook.tiles.home.controller;
+
+import java.util.Locale;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	private final Logger _log = Logger.getLogger(HomeController.class.getName());
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		model.addAttribute("url", "home");
+		return "home.tiles";
+	}
+	
+	@RequestMapping(value = "/member/home", method = RequestMethod.GET)
+	public String memberHome(Locale locale, Model model) {
+		model.addAttribute("url", "home");
+		return "member/home.tiles";
+	}
+}
